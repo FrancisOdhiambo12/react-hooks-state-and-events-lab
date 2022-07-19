b049ec81fe43b25169d8501427e2ca7c16ba76ba
Francis Odhiambo2 <francisodhiambo2@gmail.com>
2022-07-19 23:19:45 +0300

    Done.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [ itemName, setItemName ] = useState(false);
+
+  function addToCart() {
+    setItemName((itemName) => !itemName);
+  }
+
   return (
-    <li className="">
+    <li className={itemName ? "in-cart": ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToCart}>Add to Cart</button>
     </li>
   );
 }
 
 export default Item;
+
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [filterItems, setFilterItems ] = useState("Filter by category");
+
+  function filterHandler(event){
+    setFilterItems(event.target.value)
+  }
+
+const display = items.filter((item) => {
+  if(filterItems === "Filter by category"){
+    return true;
+  } else {
+    return item.category === filterItems;
+  }
+})
+
+const itemList = display.map((item) => (
+  <Item key={item.id} name={item.name} category={item.category} />
+))
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={filterHandler}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +32,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {itemList}
       </ul>
     </div>
   );
